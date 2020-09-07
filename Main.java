02517273d5867d1882bd672f16134954a20689ac
ninz2020 <70809103+ninz2020@users.noreply.github.com>
2020-09-07 15:15:10 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package lesson1;
+
+import java.net.SocketOption;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //printHelloWorld!();
+
+       /* int valA = 7;
+        int valB = 3;
+        int result;
+        result = valA / valB;
+        System.out.println(result);*/
+    }
+
+
+    int apple = 14;
+    int orange = 6;
+
+    printComparisonFruit(apple, orange);
+
+    printComparisonFruit(a:12, b:3);
+
+    printComparisonFruit(a:12, b:23);
+
+
+String str = "Hello, World!";
+
+    {
+
+        public void comparisonFruit(int a, int b) }
+
+
+
+
